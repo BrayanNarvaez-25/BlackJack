d4cdc5462e0f4ccf67ac7775d3e0e1aa06ede808
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-04-22 19:59:14 -0500

    BN: Se crea la clase Carta con sus atributos, getters y setters y su método imprimir

---
+++ src/com/krakedev/juegos/entidades/Carta.java
@@ -0,0 +1,36 @@
+package com.krakedev.juegos.entidades;
+
+public class Carta {
+	private String valor;
+	private int valorJuego;
+	private String palo;
+	
+	public String getValor() {
+		return valor;
+	}
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+	public int getValorJuego() {
+		return valorJuego;
+	}
+	public void setValorJuego(int valorJuego) {
+		this.valorJuego = valorJuego;
+	}
+	public String getPalo() {
+		return palo;
+	}
+	public void setPalo(String palo) {
+		this.palo = palo;
+	}
+	
+	public void imprimir() {
+		String mensaje = "----CARTA----" +
+				"\nValor: "+valor
+				+"\nValor Juego: "+valorJuego
+				+"\nPalo: "+palo
+				+"---------------------------";
+		System.out.println(mensaje);
+	}
+	
+}
